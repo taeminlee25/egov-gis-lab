ae3c1b4f72613c0812f5bfb00dc1ab40e3f42b67
taeminlee25 <ltm7149@gmail.com>
2026-02-12 17:33:53 +0900

    <feat>: 클래스 내 필드와 지역변수 초기화 공부

---
+++ src/main/java/sec01_field/EX02_InitialValueOfFieldAndLocalVariable/InitialValueOfFieldAndLocalVariable.java
@@ -0,0 +1,28 @@
+package sec01_field.EX02_InitialValueOfFieldAndLocalVariable;
+
+class A {
+    boolean m1;
+    int m2;
+    double m3;
+    String m4;
+
+    void printFieldValue() {
+        System.out.println(m1);
+        System.out.println(m2);
+        System.out.println(m3);
+        System.out.println(m4);
+    }
+
+    void printLocalVariable() {
+        int k;
+        // System.out.println(k);
+    }
+}
+public class InitialValueOfFieldAndLocalVariable {
+        public static void main(String[] args) {
+            // 클래스를 활용해 객체 생성
+            A a = new A();
+            // 객체 활용
+            a.printFieldValue();
+        }
+}
